bcf4c982ffdf67862932c65b7fe71f7bb5e2793c
prathamesh0 <prathamesh.musale0@gmail.com>
2023-04-28 12:49:58 +0530

    Tag lighthouse images with version for testing

--- app/data/container-build/cerc-fixturenet-eth-lighthouse/build.sh
+++ app/data/container-build/cerc-fixturenet-eth-lighthouse/build.sh
@@ -5,4 +5,4 @@ source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t cerc/fixturenet-eth-lighthouse:local -f ${SCRIPT_DIR}/Dockerfile ${build_command_args} $SCRIPT_DIR
+docker build -t cerc/fixturenet-eth-lighthouse:v4.1.0-local -f ${SCRIPT_DIR}/Dockerfile ${build_command_args} $SCRIPT_DIR
